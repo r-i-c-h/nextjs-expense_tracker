1c9352863aa413be4a361f2557dcdfc0713bf346
r-i-c-h <rich@richwerden.com>
2024-08-15 18:38:15 -0400

    adds Clerk Auth service

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
 import { Roboto } from "next/font/google";
 import "./styles/globals.css";
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Exp Tracker",
+  title: "Money Tracker",
   description: "Tracking for Expenses n' Such",
 };
 
@@ -15,8 +16,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={roboto.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={roboto.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
+
+/* Clerk Official Docs version:
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <html lang="en"> <body>
+          <SignedOut> <SignInButton /> </SignedOut>
+          <SignedIn> <UserButton /> </SignedIn>
+          {children}
+      </body> </html>
+    </ClerkProvider>
+  )
+}
+*/
