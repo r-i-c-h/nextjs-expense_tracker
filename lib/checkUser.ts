689755afda33729579a0e8ec3246191317cb95cd
r-i-c-h <rich@richwerden.com>
2024-08-28 02:22:56 -0400

    🤞 Forces default name value for new users

--- lib/checkUser.ts
+++ lib/checkUser.ts
@@ -1,33 +1,43 @@
-import { currentUser } from "@clerk/nextjs/server";
-import { db } from "@/lib/db";
+import { currentUser } from '@clerk/nextjs/server';
+import { db } from '@/lib/db';
 
 // Check Clerk Login Status
 export const checkUser = async () => {
   const user = await currentUser();
 
   // INVALID Clerk result:
-  if (!user) { return null; }
+  if (!user) {
+    return null;
+  }
 
   // VALID Clerk result ⫸⫸⫸
-  // Look for records in our Prisma / Neon Database
+  // Look for the Clerk User in our Neon Database
   const loggedInUser = await db.user.findUnique({
     where: {
       clerkUserId: user.id
     }
-  })
+  });
 
-  // IF alreay in db, return the user
-  if (loggedInUser) { return loggedInUser }
+  // IF alreay in our db, return the user
+  if (loggedInUser) {
+    return loggedInUser;
+  }
 
-  // ELSE Create new User from the clerk user data
+  // ELSE Create new a User in our Neon db from the Clerk `user` data
+
+  // Default Name if there isn't a firstName and lastName in Clerk...
+  const { firstName, lastName } = user; // <~~ The *Clerk* `user`
+  const fName = firstName ?? 'Anonymous';
+  const lName = lastName ?? 'User';
+
   const newUser = await db.user.create({
     data: {
       clerkUserId: user.id,
-      name: `${user.firstName} ${user.lastName}`,
+      name: `${fName} ${lName}`,
       imageUrl: user.imageUrl,
       email: user.emailAddresses[0].emailAddress
     }
   });
 
-  return newUser
-}
+  return newUser;
+};
